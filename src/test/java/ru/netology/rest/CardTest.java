12532f798e9c123809444a7573d99ecd070f122b
SergeyPanama <97026373+SergeyPanama@users.noreply.github.com>
2022-06-05 16:19:46 +0300

    Update CardTest.java

--- src/test/java/ru/netology/rest/CardTest.java
+++ src/test/java/ru/netology/rest/CardTest.java
@@ -1,18 +1,20 @@
 package ru.netology.rest;
 
 import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.Configuration;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.Keys;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
-import java.time.Duration;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
-import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selectors.withText;
-import static com.codeborne.selenide.Selenide.*;
 
 public class CardTest {
     
