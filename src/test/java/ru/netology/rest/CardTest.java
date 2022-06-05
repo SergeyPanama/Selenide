cb0c14935bda7d629b3dc3eeab70785cb7ec1391
SergeyPanama <97026373+SergeyPanama@users.noreply.github.com>
2022-06-05 15:56:18 +0300

    Update CardTest.java

--- src/test/java/ru/netology/rest/CardTest.java
+++ src/test/java/ru/netology/rest/CardTest.java
@@ -14,8 +14,8 @@ import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 
-
 public class CardTest {
+    
     LocalDate localDate = LocalDate.now().plusDays(4);
     DateTimeFormatter data = DateTimeFormatter.ofPattern("dd.MM.yyyy");
     String strData = localDate.format(data);
@@ -27,12 +27,11 @@ public class CardTest {
         $("[data-test-id='city'] input").val("Москва");
         $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
         $("[data-test-id='date'] input").val(strData);
-        $("[data-test-id='name'] input").val("Андрей Владимирович Селиванов");
+        $("[data-test-id='name'] input").val("Андрей Селиванов");
         $("[data-test-id='phone'] input").val("+79167411877");
         $("[data-test-id='agreement']").click();
         $(withText("Забронировать")).click();
         $("[class='notification__content']")
-                .shouldHave(Condition.text("Встреча успешно забронирована на " + (strData)), Duration.ofSeconds(15));
+            .shouldHave(Condition.text("Встреча успешно забронирована на " + (strData)), Duration.ofSeconds(15));
        }
-
     }
